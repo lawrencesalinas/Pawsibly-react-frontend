a9f0bb9f7d90fddac8a83b19a12a7a656741b27b
Lawrence <lawrencesalinas@yahoo.com>
2022-01-28 17:45:05 -0500

    userdata from server obtained

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,33 +1,93 @@
 import { useEffect, useState } from "react";
-import { getUsersAndPets } from "../api/pets";
-import UserPets from '../components/UserPets'
+import { Link } from "react-router-dom";
+import { Row, Col, Image, Card, Button } from 'react-bootstrap'
+import Footer from '../components/Footer'
+import apiUrl from "../apiConfig";
+import axios from "axios";
 
-const ProfileScreen = ({user}) => {
+
+export default function ProfileScreen(props) {
+  console.log('user here', props);
   // user data and user pet is called here
+  const [image, setImage]= useState();
   const [userPets, setUserPets] = useState([]);
   const [userData, setUserData] = useState([]);
-
+  const [trigger, setTrigger] = useState(false)
   useEffect(() => {
-    getUsersAndPets(user)
-      .then((user) => {
-        console.log(user);
-        let userInfo = user.data.user;
-        let userPet = user.data.user.pets_owned;
-        setUserPets(userPet);
-        setUserData(userInfo);
-      })
-      .catch((err) => console.error(err));
-  }, []);
+    async function fetchData() {
+      const { data } = await axios.get(`${apiUrl}/profile`);
+      setSitters(data.sitters);
+    }
+    fetchData();
+  }, [trigger]);
+
+console.log(userData);
+  const uploadPhoto= (e) => {
+
+   
+    const uploadData = new FormData()
+    uploadData.append('image', image)
+    uploadData.append('id', userData.id)
+
+ 
+ 
+     fetch('http://localhost:8000/profileImage', {
+       method: 'POST',
+       headers: {
+      
+         'Authorization': `Token ${props.user.token}`
+       },
+       body: uploadData
+     }).then(res => {
+    
+       console.log('new pehoto added',res);
+       setTrigger(x=> !x)
+     })
+       // useNavigate(-1)
+     .catch(error => {
+       console.log(error);
+     
+     })
+ 
+   }
+
 
   return (
-    <div>
-      <h1>I am the user</h1>
-      <h1>{userData.email}</h1>
-      <h2>{userData.namer}</h2>
+    <div className = 'profile'>
+    <Row>
+<Col md ={3}>
+<Card>
+<Image src = {apiUrl+userData.image} fluid/>
+<Button variant = 'success' onClick={() => uploadPhoto()}>upload photo</Button>
+<label>
+        <input type="file" onChange={(evt) => setImage(evt.target.files[0])}/>
+      </label>
+</Card>
+</Col>
 
-     
+    <Col md={3}>
+    <div className = 'profilescreen_info' >
+      <h3 class="flow-text">Hello, {userData.last_name}!</h3>
+
+
+      <Row className='profilescreen_buttons'>
+      <Link className ='link' to={`/mybookings/${props.user.id}`}><Button  variant = 'warning'>My Bookings</Button></Link>
+      <Link className ='link' to={`/myreviews/${props.user.id}`}><Button variant = 'warning'>My Reviews</Button></Link>
+      <Link className ='link' to={`/contact/${props.user.id}`}><Button variant = 'warning'>Contact sitter</Button></Link>
+      </Row>
     </div>
+    </Col>
+    <Col className="pt-5">
+    <Link  to={`/pets/`}><Button  variant = 'warning'>My Pets</Button></Link>
+    </Col>
+    <Col >
+    <i class="fas fa-paw paw fa-10x"></i>
+    </Col>
+    </Row>  
+ <Footer/>
+    </div>
+  
   );
 };
 
-export default ProfileScreen;
+
